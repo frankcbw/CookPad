396a46e7d3bb2664d4a4f0503e5e0c4a16640b30
FrankChen <frankchen0717@gmail.com>
2020-12-29 14:05:18 -0500

    upload the project

---
+++ client/build/precache-manifest.928d6388b71c8f8d15a5cd3e5d3685b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb2557ddb7c160ca393433d3c2acb7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85bd85cfe7decea26749",
+    "url": "/static/css/2.420e7b89.chunk.css"
+  },
+  {
+    "revision": "9a3d49d5cf57fd474fea",
+    "url": "/static/css/main.f1097c7c.chunk.css"
+  },
+  {
+    "revision": "85bd85cfe7decea26749",
+    "url": "/static/js/2.925cee5d.chunk.js"
+  },
+  {
+    "revision": "d10a9f1e2ff47e0730e9402437b475b6",
+    "url": "/static/js/2.925cee5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3d49d5cf57fd474fea",
+    "url": "/static/js/main.5e1ad946.chunk.js"
+  },
+  {
+    "revision": "965ed4d46f1edaa1cbdd",
+    "url": "/static/js/runtime-main.6a44330e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "efafcbc792f117355e50ff5f2dd1cdb2",
+    "url": "/static/media/noodles.efafcbc7.png"
+  },
+  {
+    "revision": "ad998962c23c33657d2745edea0298e3",
+    "url": "/static/media/salad.ad998962.png"
+  },
+  {
+    "revision": "cc032ac8e64cd5decd48f05f1320651c",
+    "url": "/static/media/sandwich.cc032ac8.png"
+  },
+  {
+    "revision": "a2862b82819d83399374bcf12edb3198",
+    "url": "/static/media/seafood.a2862b82.png"
+  },
+  {
+    "revision": "f287703cae6e078cf871b688d1359a56",
+    "url": "/static/media/soup.f287703c.png"
+  }
+]);
